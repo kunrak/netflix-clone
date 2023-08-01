8b3b6adfb78d768d946ec5cacfa021e46d1c5f78
kunrak <rakeshkunducsgi@gmail.com>
2023-08-01 10:38:10 +0530

    .

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,45 @@
+import { Adb } from '@mui/icons-material';
+import { AppBar, Avatar, Box, Button, Container, IconButton, Menu, MenuItem, Toolbar, Tooltip, Typography } from '@mui/material';
+import * as React from 'react';
+import MenuIcon from '@mui/icons-material/Menu';
+import { Link } from 'react-router-dom';
+
+const pages = ['TV Shows', 'Movies', 'Recently Added', 'My List'];
+const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+
+function Header() {
+    const [anchorElNav, setAnchorElNav] = React.useState(null);
+    const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+    const handleOpenNavMenu = (event) => {
+        setAnchorElNav(event.currentTarget);
+    };
+    const handleOpenUserMenu = (event) => {
+        setAnchorElUser(event.currentTarget);
+    };
+
+    const handleCloseNavMenu = () => {
+        setAnchorElNav(null);
+    };
+
+    const handleCloseUserMenu = () => {
+        setAnchorElUser(null);
+    };
+
+    return (
+        <AppBar position="static" sx={{ backgroundColor: 'black' }}>
+            <Container maxWidth="xl">
+                <Toolbar disableGutters>
+                    <Box mr={10}>
+                        <img src="https://github.com/meabhisingh/netflix/blob/master/src/logo.png?raw=true" alt='Netflix Logo' height={30} />
+                    </Box>
+                    <Link to="/tvshows" style={{ textDecoration: 'none', color: 'white', marginRight: '4%' }}>TV Shows</Link>
+                    <Link to="/tvshows" style={{ textDecoration: 'none', color: 'white', marginRight: '4%' }}>TV Shows</Link>
+                    <Link to="/tvshows" style={{ textDecoration: 'none', color: 'white', marginRight: '4%' }}>TV Shows</Link>
+                    <Link to="/tvshows" style={{ textDecoration: 'none', color: 'white', marginRight: '4%' }}>TV Shows</Link>
+                </Toolbar>
+            </Container>
+        </AppBar>
+    );
+}
+export default Header;
